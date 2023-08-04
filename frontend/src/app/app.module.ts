734eedf63c7e546f8ca82f2e1f7949219e64952a
Bosnan_Lab <121900154+bosnan-lab@users.noreply.github.com>
2023-08-04 14:24:18 -0600

    installation admin template in angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { routing } from './app.routing';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
+  declarations: [AppComponent, DashboardComponent, SidebarComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, routing],
   providers: [],
   bootstrap: [AppComponent],
