5aa9e3e6e5469cb8e8e6ae623f0bfbd705139f5b
Bosnan_Lab <121900154+bosnan-lab@users.noreply.github.com>
2023-08-06 13:27:31 -0600

    first api call in angular frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,8 +10,19 @@ import { routing } from './app.routing';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, SidebarComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, routing],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    routing,
+    FormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TestService } from '../../services/test.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  constructor(private _testService: TestService) {}
+  ngOnInit(): void {
+    this._testService.prueba_test().subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
